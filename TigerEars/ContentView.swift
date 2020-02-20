958b82a091bdcd2d39625e8ea40d9985d3f6179c
Zohra18 <ach.zohra@gmail.com>
2020-02-20 16:18:43 +0100

    Initial Commit

---
+++ TigerEars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TigerEars
+//
+//  Created by Zohra Achour on 20/02/2020.
+//  Copyright © 2020 Zohra Achour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
