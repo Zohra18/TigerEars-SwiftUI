6c1910528b3f42ac19e814a016b4560354c6120b
Zohra18 <ach.zohra@gmail.com>
2020-02-20 21:29:50 +0100

    main view creation

--- TigerEars/ContentView.swift
+++ TigerEars/ContentView.swift
@@ -10,9 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        MainView()
     }
 }
+
+
+struct MainView: View {
+    
+    // obligé afin de pouvoir voir la View
+    var body: some View {
+        
+        // on créer un index de profondeur
+        ZStack {
+            // fond d'écran
+            LinearGradient(gradient: Gradient(colors: [Color("darkPurple"), Color("mainPurple"), Color("mainPink"), Color("lightPink")]), startPoint: .top, endPoint: .bottom)
+                // ignore toute la Safe Area
+                .edgesIgnoringSafeArea(.all)
+            
+            // englobe nos composants
+            VStack {
+                Text("TAP TO RECORD")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.light)
+                
+                Image(systemName: "arrowtriangle.down.fill")
+                    .resizable()
+                    .frame(width:30, height: 30)
+                    .foregroundColor(.white)
+                
+            }
+            
+            
+        }// fin de zstack
+        
+    }// fin de body
+}// fin de MainView
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
