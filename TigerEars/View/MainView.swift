da17488ac63d1841f6d112d0a16f8ca42cad3159
Zohra18 <ach.zohra@gmail.com>
2020-02-21 19:56:45 +0100

    Added a new file in order to deconstruct the mainView further

---
+++ TigerEars/View/MainView.swift
@@ -0,0 +1,51 @@
+//
+//  MainView.swift
+//  TigerEars
+//
+//  Created by Zohra Achour on 21/02/2020.
+//  Copyright © 2020 Zohra Achour. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State var animationAmount: CGFloat = 1
+    @State var isRecording: Bool = false
+    
+    // we always use this syntax in order to declare a View
+    var body: some View {
+        
+        // ZStack allow to stack components on top of one another
+        ZStack {
+            // background of our View
+            LinearGradient(gradient: Gradient(colors: [Color("darkBrown"), Color("darkOrange"), Color("mainOrange"), Color("lightOrange")]), startPoint: .top, endPoint: .bottom)
+                // ignore toute la Safe Area
+                .edgesIgnoringSafeArea(.all)
+            
+            // VStack gather our components on a vertical axis
+            VStack {
+                Text("TAP TO RECORD")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.light)
+                
+                Image(systemName: "arrowtriangle.down.fill")
+                    .resizable()
+                    .frame(width:30, height: 30)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 50)
+                
+// Button made from the ButtonView
+                ButtonView(animationAmount: animationAmount, isRecording: isRecording)
+                
+            }// end of VStack
+        }// end of ZStack
+    }// end of body
+}// end of MainView
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
